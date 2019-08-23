0298b702884cf82feaea6e762c03712c5a0e22c0
Manoela <manozulian@gmail.com>
2019-08-23 18:17:27 -0300

    added relationship to pet and guardian

--- app/models/guardian.rb
+++ app/models/guardian.rb
@@ -1,3 +1,4 @@
 class Guardian < ApplicationRecord
     validates :name, presence: true
+    has_many :pets
 end
--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,3 +1,4 @@
 class Pet < ApplicationRecord
     validates :name, presence: true
+    belongs_to :guardian 
 end
